adf278a6844dfd45ff3ada41ece7f13c85587fbc
Omar Adel <omaeradel1999@gmail.com>
2024-10-24 13:44:19 +0300

    finished Trainer and TrainerDB

---
+++ gymproject/Trainer.java
@@ -0,0 +1,44 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package gymproject;
+
+
+public class Trainer {
+    private String TrainerId;
+    private String Name;
+    private String Speciality;
+    private String Email;
+    private String PhoneNumber;
+
+    public Trainer(String TrainerId, String Name, String Speciality, String Email, String PhoneNumber) {
+        this.TrainerId = TrainerId;
+        this.Name = Name;
+        this.Speciality = Speciality;
+        this.Email = Email;
+        this.PhoneNumber = PhoneNumber;
+    }
+    
+    public Trainer(String[] SplittedData){
+    this.TrainerId=SplittedData[0];
+    this.Name=SplittedData[1];
+    this.Speciality=SplittedData[2];
+    this.Email=SplittedData[3];
+    this.PhoneNumber=SplittedData[4];
+    
+    }
+    
+    public String toString() {
+     return TrainerId + ", " + Name + ", " + Speciality + ", " + Email + ", " + PhoneNumber;
+}
+    
+    public String LineRepresentation(){
+        return this.toString();
+    }
+    
+    public String getSearchKey(){
+    return this.TrainerId;
+    }
+    
+}
---
+++ gymproject/TrainerDatabase.java
@@ -0,0 +1,100 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package gymproject;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileWriter;
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class TrainerDatabase {
+
+    private String filename;
+    private ArrayList<Trainer> records = new ArrayList<>();
+
+    public TrainerDatabase(String filename) {
+        this.filename = filename;
+        this.readFromFile();
+
+    }
+
+    public void readFromFile() {
+        File myFile = new File(this.filename);
+
+        try {
+            Scanner myReader = new Scanner(myFile);
+            while (myReader.hasNextLine()) {
+                String[] SplittedData = createRecordFrom(myReader.nextLine());
+                this.records.add(new Trainer(SplittedData));
+            }
+            myReader.close();
+        } catch (FileNotFoundException e) {
+            System.out.println("An error occurred while reading the file: " + e.getMessage());
+        }
+    }
+
+    public String[] createRecordFrom(String Line) {
+        return Line.split(", ");
+    }
+
+    public ArrayList<Trainer> returnAllRecords() {
+        return this.records;
+    }
+
+    public boolean contains(String Key) {
+
+        if (getRecord(Key) != null) {
+            return true;
+        }
+        System.out.println("trainer with key not found");
+        return false;
+    }
+
+    public Trainer getRecord(String Key) {
+
+        for (Trainer current : this.records) {
+            if (current.getSearchKey() == Key) {
+                return current;
+            }
+        }
+        System.out.println("record with key not found");
+        return null;
+    }
+
+    public void insertRecord(Trainer Record) {
+        if (this.records.contains(Record.getSearchKey())) {
+            System.out.println("Trainer Already Exists");
+            return;
+        }
+        this.records.add(Record);
+    }
+
+    public void deleteRecord(String Key) {
+        if (this.contains(Key)) {
+            this.records.remove(this.getRecord(Key));
+            return;
+        }
+        System.out.println("Unable to delete Trainer,Key doesn't exist");
+    }
+
+    public void saveToFile() {
+
+        try {
+
+            FileWriter output = new FileWriter(this.filename);
+            int i = 0;
+            while (this.records.get(i) != null) {
+                output.write(this.records.get(i).toString());
+                i++;
+            }
+            output.close();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+    }
+
+}
