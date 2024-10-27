42d5bebaa117ee13540a0fdc613bf3bd76ce72ba
Mazen Azhary <mazen.mazen9850@gmail.com>
2024-10-27 21:32:32 +0300

    merged omar's changes with some of my uncommitted work

--- gymproject/MemberDatabase.java
+++ gymproject/MemberDatabase.java
@@ -1,15 +1,9 @@
 package gymproject;
 
-import java.io.File;
-import java.io.FileNotFoundException;
-import java.io.FileWriter;
-import java.util.ArrayList;
-import java.util.Scanner;
-
 public class MemberDatabase extends DataBases {
 
     public MemberDatabase() {
-        super("C:\\Users\\Etijah\\Documents\\NetBeansProjects\\GymProject\\src\\gymproject\\Members.txt");
+        super("src/gymproject/Members.txt");
     }
     public Users createRecordFrom(String Line) {
         return new Member(Line.split(", "));
